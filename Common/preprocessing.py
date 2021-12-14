221b83a479612de373d487ec8adfef11d27baf5f
cshablin <Carmel.Shablin@kla-tencor.com>
2021-12-14 09:51:57 +0200

    added preprocess() signature

--- Common/preprocessing.py
+++ Common/preprocessing.py
@@ -1,1 +1,15 @@
-from typing import Tuple, Dict, List  #
+import pandas as pd
+from typing import Tuple, Dict, List, Optional  #
+
+
+def preprocess(df: pd.DataFrame) -> pd.DataFrame:
+    pass
+
+
+# Example for using typing, very convenient for IntelliSense
+def foo(dfs: List[pd.DataFrame]) -> Optional[Dict[int, pd.DataFrame]]:
+    result = {}
+    i = 0
+    for df in dfs:
+        result[i] = df
+    return result
