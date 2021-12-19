98ee5e6a926d8abf9e06438d9f8ea67df53fa42d
cshablin <Carmel.Shablin@kla-tencor.com>
2021-12-19 22:08:06 +0200

    fix preprocessing.py now we have 100 columns

--- Common/preprocessing.py
+++ Common/preprocessing.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 #load training dataset
-df_train = pd.read_csv("data/WADI_14days_new.csv")
+df_train = pd.read_csv("../Data/WADI_14days_new.csv")
 
 print(df_train.head())
 
@@ -28,13 +28,27 @@ print(len(df_train.columns))
 
 #removing columns with constant value
 
+Train const columns:
+['1_LS_001_AL', '1_LS_002_AL', '1_MV_002_STATUS', '1_MV_003_STATUS', '1_P_002_STATUS', '1_P_004_STATUS', '1_P_006_STATUS', '2_MCV_007_CO', '2_MV_001_STATUS', '2_MV_002_STATUS', '2_MV_004_STATUS', '2_MV_005_STATUS', '2_MV_009_STATUS', '2_P_004_STATUS', '2_PIC_003_SP', '2_SV_101_STATUS', '2_SV_201_STATUS', '2_SV_301_STATUS', '2_SV_401_STATUS', '2_SV_501_STATUS', '2_SV_601_STATUS', '3_LS_001_AL', '3_MV_001_STATUS', '3_MV_002_STATUS', '3_MV_003_STATUS', '3_P_001_STATUS', '3_P_002_STATUS', '3_P_003_STATUS', '3_P_004_STATUS', 'PLANT_START_STOP_LOG']
+
+Test const columns: 
+['1_LS_001_AL', '1_LS_002_AL', '1_P_002_STATUS', '1_P_004_STATUS', '2_MV_001_STATUS', '2_MV_002_STATUS', '2_MV_004_STATUS', '2_MV_005_STATUS', '2_MV_009_STATUS', '2_P_004_STATUS', '2_SV_101_STATUS', '2_SV_201_STATUS', '2_SV_301_STATUS', '2_SV_401_STATUS', '2_SV_501_STATUS', '2_SV_601_STATUS', '3_AIT_001_PV', '3_AIT_002_PV', '3_LS_001_AL', '3_MV_001_STATUS', '3_MV_002_STATUS', '3_MV_003_STATUS', '3_P_001_STATUS', '3_P_002_STATUS', '3_P_003_STATUS', '3_P_004_STATUS', 'PLANT_START_STOP_LOG']
+
+
+In Train not in Test constant columns:
+1_MV_002_STATUS, 1_MV_003_STATUS, 1_P_006_STATUS, 2_MCV_007_CO,  2_PIC_003_SP
+
 """
+in_train_not_in_test_const_col = ['1_MV_002_STATUS', '1_MV_003_STATUS', '1_P_006_STATUS', '2_MCV_007_CO',  '2_PIC_003_SP']
 constant_value_columns = []
 for column in df_train.columns:
     if(df_train[column].nunique()==1):
         constant_value_columns.append(column)
 
-df_train = df_train.loc[:, df_train.apply(pd.Series.nunique) != 1]
+constant_value_columns = list(set(constant_value_columns) - set(in_train_not_in_test_const_col))
+
+# df_train = df_train.loc[:, df_train.apply(pd.Series.nunique) != 1]
+df_train = df_train.drop(constant_value_columns, axis=1)
 
 print(len(df_train.columns))
 
@@ -44,7 +58,7 @@ print(len(df_train.columns))
 
 """
 
-#we are left with 95 columns - out of which two are identifiers - "Date", "Time"
+#we are left with 100 columns - out of which two are identifiers - "Date", "Time"
 
 """
 
@@ -54,7 +68,7 @@ print(len(set_of_empty_vals[0]))
 """
 
 # we have just 34 rows where we have empty values. I recommend dropping them. No need to impute for the values because 34 rows is negligible when compared to 784537 rows
-
+for example rows 61703~61712 missing value in same column '2B_AIT_004_PV' which makes it hard to impute 
 """
 df_train = df_train.drop(set_of_empty_vals[0])
 
@@ -62,7 +76,7 @@ df_train = df_train.drop(set_of_empty_vals[0])
 """
 ################### Repeat process for test set #######################
 """
-df_test = pd.read_csv("data/WADI_attackdataLABLE.csv")
+df_test = pd.read_csv("../Data/WADI_attackdataLABLE.csv")
 
 
 print(df_test.head())
@@ -97,7 +111,7 @@ print(len(df_test.columns))
 
 """
 
-#we are left with 95 columns - out of which two are identifiers - "Date", "Time"
+#we are left with 100 columns (+1 with label, the last one) - out of which two are identifiers - "Date", "Time"
 
 """
 
@@ -106,11 +120,11 @@ print(len(set_of_empty_vals[0]))
 
 """
 
-# we have just 194 rows where we have empty values in the test set. we can safely remove them from our evaluation.
+# we have just 2 (172801, 172802) rows (all empty) in the test set. we can safely remove them from our evaluation.
 
 """
 df_test = df_test.drop(set_of_empty_vals[0])
 
-df_train.to_csv("data/Cleaned_Trainset.csv")
+df_train.to_csv("../Data/Cleaned_Trainset.csv")
 
-df_test.to_csv("data/Cleaned_Testset.csv")
+df_test.to_csv("../Data/Cleaned_Testset.csv")
