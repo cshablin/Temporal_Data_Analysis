8490fc849b851cfd13fdb5b31ee4ceba36afcf2d
cshablin <Carmel.Shablin@kla-tencor.com>
2021-12-23 17:17:33 +0200

    test_with_multivariate runs on 1 column

--- Common/data_preparation.py
+++ Common/data_preparation.py
@@ -22,19 +22,14 @@ def split_time_series_data(column: pd.Series, step: int = 500, sample_size: int 
 
 def split_time_series_with_labels(columns_4_split: pd.DataFrame, labels: np.ndarray, step: int = 500, sample_size: int = 1000) \
         -> Tuple[np.ndarray, np.ndarray]:
-    '''
+    """
 
     :param columns_4_split: shaped (n_timestamps, n_columns)
     :param labels: column with labels
     :param step: step size in timestamps between adjacent samples
     :param sample_size: also sliding window size
     :return: x_matrix shaped (sample_size, n_columns, ~ n_timestamps\\step), y_vector shaped (~ n_timestamps\\step,)
-    '''
-    import collections
-
-    # TODO: smart usage of the scarce negative samples, we can do small steps when arriving to negative sample. This way
-    #  we will produce more negative sample in result
-    # Counter({1: 162824, -1: 9977})
+    """
     df_np_arr = columns_4_split.values
     chunks = []
     result_labels = []
@@ -45,5 +40,53 @@ def split_time_series_with_labels(columns_4_split: pd.DataFrame, labels: np.ndar
         to_index = index + sample_size
         chunks.append(df_np_arr[index: to_index, :])
         result_labels.append(labels[to_index])
+    stacked_arrays = np.stack(chunks, axis=2)
+    return stacked_arrays, np.array(result_labels)
+
+
+def split_time_series_with_negative_labels(columns_4_split: pd.DataFrame,
+                                           labels: np.ndarray, step: int = 500, sample_size: int = 1000,
+                                           min_negative_last_chunk_size: int = 20) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    :param columns_4_split: shaped (n_timestamps, n_columns)
+    :param labels: column with labels
+    :param step: step size in timestamps between adjacent samples
+    :param sample_size: also sliding window size
+    :param min_negative_last_chunk_size: num of negative timestamps in succession at the end of returned samples
+    :return: x_matrix shaped (sample_size, n_columns, ~ n_timestamps\\step), y_vector shaped (~ n_timestamps\\step,)
+    """
+    # import collections
+    # smart usage of the scarce negative samples, do small steps when arriving to negative sample. This way
+    # we will produce more negative sample in result
+    # Counter({1: 162824, -1: 9977})
+    negative_indexes = np.where(labels == -1)[0]
+
+    df_np_arr = columns_4_split.values
+    chunks = []
+    result_labels = []
+    # last_index = df_np_arr.shape[0] - 1
+    for i in range(0, negative_indexes.shape[0], step):
+        # check if previous indices are successors - fast hack
+        cur_neg_index = negative_indexes[i]
+        if cur_neg_index - negative_indexes[i - min_negative_last_chunk_size] == min_negative_last_chunk_size:
+            chunks.append(df_np_arr[cur_neg_index - sample_size: cur_neg_index, :])
+            result_labels.append(-1)
+
+        # temp_neg_index = cur_neg_index
+        # succession = True
+        # for j in range(min_negative_last_chunk_size - 1):
+        #     prev_neg_index = negative_indexes[i - (j + 1)]
+        #     if temp_neg_index - prev_neg_index != 1:
+        #         succession = False
+        #         break
+        #     temp_neg_index = prev_neg_index
+        # if succession:
+        #     chunks.append(df_np_arr[cur_neg_index - sample_size: cur_neg_index, :])
+        #     result_labels.append(-1)
+    # for i in range(0, negative_indexes.shape[0], min_negative_last_chunk_size):
+    #     if i % min_negative_last_chunk_size == min_negative_last_chunk_size - 1:
+    #         neg_index = negative_indexes[i]
+    #         chunks.append(df_np_arr[neg_index - sample_size: neg_index, :])
+    #         result_labels.append(-1)
     stacked_arrays = np.stack(chunks, axis=2)
     return stacked_arrays, np.array(result_labels)
--- Exp_Shapelets/tests.py
+++ Exp_Shapelets/tests.py
@@ -3,8 +3,11 @@ import numpy
 import numpy as np
 import pandas as pd
 from gendis.genetic import GeneticExtractor
-from typing import List, Tuple
-from Common.data_preparation import split_time_series_data, split_time_series_with_labels
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from typing import List
+from Common.data_preparation import split_time_series_data, split_time_series_with_labels, \
+    split_time_series_with_negative_labels
 
 
 class ShapeletsTestCase(unittest.TestCase):
@@ -41,7 +44,27 @@ class ShapeletsTestCase(unittest.TestCase):
         self.assertEqual(True, False)
 
     def test_with_multivariate(self):
-        x_train, x_test, y_train, y_test = self.__prepare_train_test_data(['1_AIT_001_PV', '1_AIT_002_PV'], 500, 1000)
+        columns = ['1_AIT_001_PV', '1_AIT_002_PV']
+        step = 500
+        window_length = 1000
+        step4negative = 50
+        min_negative_last_chunk_size = 5
+        x_train, x_test, y_train, y_test = self.__prepare_train_test_data(columns, step, window_length, step4negative, min_negative_last_chunk_size)
+        for i in range(len(columns)):
+            print("start GENDIS for column '{0}'".format(columns[i]))
+            x_c_train, x_c_test = x_train[:, i, :].T, x_test[:, i, :].T  # shape (x_n_ts, window_length)
+            # Fit the GeneticExtractor and construct distance matrix
+            genetic_extractor = GeneticExtractor(population_size=5, iterations=10, verbose=True, n_jobs=8,
+                                                 mutation_prob=0.3, crossover_prob=0.3,
+                                                 wait=5, max_len=len(x_c_train) // 2)
+            shapelets = genetic_extractor.fit(x_c_train, y_train)
+            distances_train = genetic_extractor.transform(x_c_train)
+            distances_test = genetic_extractor.transform(x_c_test)
+
+            # Fit ML classifier on constructed distance matrix
+            lr = LogisticRegression()
+            lr.fit(distances_train, y_train)
+            print('Accuracy = {}'.format(accuracy_score(y_test, lr.predict(distances_test))))
 
     def test_random_shapelets(self):
         from tslearn.generators import random_walk_blobs
@@ -56,7 +79,11 @@ class ShapeletsTestCase(unittest.TestCase):
         lr.score(distances, y)
         self.assertEqual(1, lr.score(distances, y))
 
-    def __prepare_train_test_data(self, columns: List[str], step: int = 500, time_window: int = 1000):
+    def __prepare_train_test_data(self, columns: List[str], step: int = 500, time_window: int = 1000, step4negative: int = 5, min_negative_last_chunk_size: int = 20):
+        negative_label_multivariate_split_data, neg_y = split_time_series_with_negative_labels(self.mixed_labels_df[columns],
+                                                                                        self.mixed_labels_df['Attack LABLE (1:No Attack, -1:Attack)'],
+                                                                                        step4negative, time_window, min_negative_last_chunk_size)
+
         # note we can't do split_time_series_with_labels() on combined DF of both dataset as they aren't chronological.
         mixed_label_multivariate_split_data, mixed_y = split_time_series_with_labels(self.mixed_labels_df[columns],
                                                                                      self.mixed_labels_df['Attack LABLE (1:No Attack, -1:Attack)'],
@@ -65,9 +92,12 @@ class ShapeletsTestCase(unittest.TestCase):
                                                                                    np.ones(self.normal_labels_df.size),
                                                                                    step, time_window)
         # number of samples is the third dimension
-        all_multivariate_prepared_x = np.concatenate((norm_label_multivariate_split_data, mixed_label_multivariate_split_data), axis=2)
-        all_multivariate_y = np.append(norm_y, mixed_y)
+        all_multivariate_prepared_x = np.concatenate((norm_label_multivariate_split_data,
+                                                      mixed_label_multivariate_split_data,
+                                                      negative_label_multivariate_split_data), axis=2)
+        all_multivariate_y = np.concatenate((norm_y, mixed_y, neg_y), axis=0)
         # manual shuffle
+        np.random.seed(123)
         permutation = np.random.permutation(all_multivariate_prepared_x.shape[2])
         np.take(all_multivariate_prepared_x, permutation, axis=2, out=all_multivariate_prepared_x)
         np.take(all_multivariate_y, permutation, axis=0, out=all_multivariate_y)
