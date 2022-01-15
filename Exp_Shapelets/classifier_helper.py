a10c3c467aec2c0bd54b4f82b70f4ae49cd20ea3
cshablin <Carmel.Shablin@kla-tencor.com>
2022-01-15 15:03:32 +0200

    added more clf tests

--- Exp_Shapelets/classifier_helper.py
+++ Exp_Shapelets/classifier_helper.py
@@ -1,14 +1,21 @@
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, AdaBoostClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.pipeline import Pipeline
+from sklearn.metrics import f1_score, make_scorer
 import numpy as np
+from sklearn.svm import SVC
+
+random_state = 10
+
+f1 = make_scorer(f1_score, average='macro')
 
 
 def get_forest_pipe_grid() -> GridSearchCV:
     forest_pipe = Pipeline([
-        ('classifier' , RandomForestClassifier()),
+        ('classifier' , RandomForestClassifier(random_state=random_state)),
     ])
 
     param_grid ={
@@ -17,34 +24,111 @@ def get_forest_pipe_grid() -> GridSearchCV:
         'classifier__max_depth' : list(range(5, 7, 1)),
     }
 
-    return GridSearchCV(forest_pipe, param_grid=param_grid, cv=5, verbose=True, n_jobs=-1)
+    return GridSearchCV(forest_pipe, param_grid=param_grid, cv=5, verbose=True, n_jobs=-1, scoring=f1)
 
 
 def get_logit_pipe_grid():
     logistic_pipe = Pipeline([
-        ('classifier' , LogisticRegression()),
+        ('classifier' , LogisticRegression(random_state=random_state)),
     ])
 
     param_grid ={
         'classifier__penalty' : ['l1', 'l2'], # , 'elasticnet'
-        'classifier__C' : np.logspace(-4, 4, 10)[0:7],
+        'classifier__C' : np.logspace(-4, 4, 10)[2:7],
         'classifier__solver' : ['lbfgs', 'liblinear'], # , 'saga'
         # 'classifier__l1_ratio' : [None, 0.1],
     }
 
-    return GridSearchCV(logistic_pipe, param_grid = param_grid, cv=5, verbose=True, n_jobs=-1)
+    return GridSearchCV(logistic_pipe, param_grid = param_grid, cv=5, verbose=True, n_jobs=-1, scoring=f1)
 
 
 def get_nn_pipe_grid():
     pipe = Pipeline([
-        ('classifier', MLPClassifier(solver='adam',max_iter = 100, early_stopping=True,verbose=0, random_state=1))
+        ('classifier', MLPClassifier(solver='adam',max_iter = 100, early_stopping=True,verbose=0, random_state=random_state))
     ])
     param_grid = {
-        'classifier__hidden_layer_sizes': [(64,32,16,8)], # (64,),(8,8) BEST (64,16)
-        'classifier__alpha': np.logspace(-2, -0.1, 4),
+        'classifier__hidden_layer_sizes': [(64,32,16,8)], # (64,),(8,8) BEST (64,32,16,8)
+        'classifier__alpha': [0.0001, 0.0005, 0.002],  # np.logspace(-4, -2, 4)
         'classifier__solver': ['adam'], # ,'lbfgs', 'sgd'
         'classifier__batch_size': [128],
         'classifier__early_stopping': [True], #  False
         'classifier__learning_rate': ['adaptive'], # 'invscaling', 'constant','adaptive'
     }
-    return GridSearchCV(pipe, param_grid, cv=5, verbose=0, n_jobs=-1)
+    return GridSearchCV(pipe, param_grid, cv=5, verbose=0, n_jobs=-1, scoring=f1)
+
+
+def get_svm_pipe_grid():
+    tuned_parameters = [
+        # {"kernel": ["rbf"], "gamma": [1e-3, 1e-4, 'scale'], "C": [1, 10, 100, 1000]}, # , 'auto'
+        # {"kernel": ["linear"], "C": [1, 10, 100, 1000]},
+        {"kernel": ["poly"], "C": [10, 100, 1000], 'degree': [2, 3], "gamma": [1e-3, 1e-4, 'scale']}, # 10, 100, 1000
+        # {"kernel": ["poly"], "C": [100, 1000], 'degree': [3,4], "gamma": ['scale']}, # 10, 100, 1000
+    ]
+    # return GridSearchCV(SVC(probability=True,random_state=random_state), tuned_parameters, cv=5, verbose=0, n_jobs=-1, scoring=f1)
+    return RandomizedSearchCV(SVC(probability=True,random_state=random_state), tuned_parameters, random_state=random_state, cv=None, verbose=0, n_jobs=-1, scoring=f1)
+
+
+def get_gb_pipe_grid():
+    tuned_parameters = [
+        # {
+        #     "loss":["deviance"],
+        #     "learning_rate": [0.01, 0.025, 0.05, 0.075, 0.1, 0.15, 0.2],
+        #     "min_samples_split": np.linspace(0.1, 0.5, 12),
+        #     "min_samples_leaf": np.linspace(0.1, 0.5, 12),
+        #     "max_depth":[3,5,8],
+        #     "max_features":["log2","sqrt"],
+        #     "criterion": ["friedman_mse",  "mae"],
+        #     "subsample":[0.5, 0.618, 0.8, 0.85, 0.9, 0.95, 1.0],
+        #     "n_estimators":[10]
+        # },
+        {
+            "loss":["deviance"],
+            "learning_rate": [0.01, 0.025, 0.05, 0.075, 0.1, 0.15, 0.2],
+            "min_samples_split": np.linspace(0.1, 0.5, 12),
+            "min_samples_leaf": np.linspace(0.1, 0.5, 12),
+            "max_depth":[3,5,8],
+            "max_features":["log2","sqrt"],
+            "criterion": ["friedman_mse",  "mae"],
+            "subsample":[0.5, 0.618, 0.8, 0.85, 0.9, 0.95, 1.0],
+            "n_estimators":[50]
+        },
+        # {
+        #     "loss":["deviance"],
+        #     "learning_rate": [0.01, 0.05, 0.075, 0.1, 0.2],
+        #     "min_samples_split": np.linspace(0.1, 0.5, 6),
+        #     "min_samples_leaf": np.linspace(0.1, 0.5, 6),
+        #     "max_depth":[5,7,8],
+        #     "max_features":["log2","sqrt"],
+        #     "criterion": ["friedman_mse"],
+        #     "subsample":[0.5, 0.8, 1.0],
+        #     "n_estimators":[10]
+        # },
+    ]
+    return RandomizedSearchCV(GradientBoostingClassifier(random_state=random_state), tuned_parameters, random_state=random_state, cv=None, verbose=0, n_jobs=-1, scoring=f1)
+    # return GridSearchCV(GradientBoostingClassifier(random_state=random_state), tuned_parameters, cv=None, verbose=0, n_jobs=-1, scoring=f1)
+
+
+def get_knn_pipe_grid():
+    tuned_parameters = [
+        {"n_neighbors": [7, 9, 15],  # 7, 9, 15
+         "weights": ['uniform', 'distance'], # 'uniform',
+         'metric': ['minkowski']}, # 10, 100, 100
+    ]
+    return RandomizedSearchCV(KNeighborsClassifier(), tuned_parameters, random_state=random_state, cv=None, verbose=0, n_jobs=-1, scoring=f1)
+
+
+def get_ada_boost_pipe_grid():
+    tuned_parameters = [
+        {
+         #    "base_estimator__criterion" : ["gini", "entropy"],
+         # "base_estimator__splitter" :   ["best", "random"],
+         # 'base_estimator__max_depth':[i for i in range(2, 11, 2)],
+         # 'base_estimator__min_samples_leaf':[5, 10],
+         "n_estimators": [100],
+         "algorithm": ['SAMME'], # , 'SAMME.R'
+         'learning_rate':[0.01, 0.1, 0.5],
+         }
+
+    ]
+    # return RandomizedSearchCV(AdaBoostClassifier(random_state=random_state), tuned_parameters, random_state=random_state, cv=None, verbose=0, n_jobs=-1, scoring=f1)
+    return GridSearchCV(AdaBoostClassifier(random_state=random_state), tuned_parameters, cv=None, verbose=0, n_jobs=-1, scoring=f1)
--- Exp_Shapelets/test_clf.py
+++ Exp_Shapelets/test_clf.py
@@ -1,13 +1,11 @@
 import os
 import unittest
 import warnings
-import numpy as np
 import pandas as pd
 
-from typing import List
-from Common.data_preparation import split_time_series_with_labels, split_time_series_with_negative_labels
 from Exp_Shapelets.Config import ShapeletsConfig
-from Exp_Shapelets.classifier_helper import get_logit_pipe_grid, get_nn_pipe_grid, get_forest_pipe_grid
+from Exp_Shapelets.classifier_helper import get_logit_pipe_grid, get_nn_pipe_grid, get_forest_pipe_grid, \
+    get_svm_pipe_grid, get_gb_pipe_grid, get_knn_pipe_grid, get_ada_boost_pipe_grid
 from Exp_Shapelets.discovery_of_shapelets import MultiVarShapeletsExtractor
 
 
@@ -19,68 +17,7 @@ def ignore_warnings(test_func):
     return do_test
 
 
-class ClassifierTestCase1(unittest.TestCase):
-
-    def __prepare_train_test_data(self, columns: List[str], step: int = 500, time_window: int = 1000, step4negative: int = 5, min_negative_last_chunk_size: int = 20):
-        negative_label_multivariate_split_data, neg_y = split_time_series_with_negative_labels(self.mixed_labels_df[columns],
-                                                                                               self.mixed_labels_df['Attack LABLE (1:No Attack, -1:Attack)'],
-                                                                                               step4negative, time_window, min_negative_last_chunk_size)
-
-        # note we can't do split_time_series_with_labels() on combined DF of both dataset as they aren't chronological.
-        mixed_label_multivariate_split_data, mixed_y = split_time_series_with_labels(self.mixed_labels_df[columns],
-                                                                                     self.mixed_labels_df['Attack LABLE (1:No Attack, -1:Attack)'],
-                                                                                     step, time_window)
-        norm_label_multivariate_split_data, norm_y = split_time_series_with_labels(self.normal_labels_df[columns],
-                                                                                   np.ones(self.normal_labels_df.size),
-                                                                                   step, time_window)
-        # number of samples is the third dimension
-        all_multivariate_prepared_x = np.concatenate((norm_label_multivariate_split_data,
-                                                      mixed_label_multivariate_split_data,
-                                                      negative_label_multivariate_split_data), axis=2)
-        all_multivariate_y = np.concatenate((norm_y, mixed_y, neg_y), axis=0)
-        # manual shuffle
-        np.random.seed(123)
-        permutation = np.random.permutation(all_multivariate_prepared_x.shape[2])
-        np.take(all_multivariate_prepared_x, permutation, axis=2, out=all_multivariate_prepared_x)
-        np.take(all_multivariate_y, permutation, axis=0, out=all_multivariate_y)
-
-        # split into 2 equal portions
-        if all_multivariate_prepared_x.shape[2] % 2 == 1:
-            all_multivariate_prepared_x = all_multivariate_prepared_x[:, :, :-1]
-            all_multivariate_y = all_multivariate_y[:-1]
-        all_x_split_arr = np.dsplit(all_multivariate_prepared_x, 2)
-        x_train = all_x_split_arr[0]
-        x_test = all_x_split_arr[1]
-        all_y_split_arr = np.hsplit(all_multivariate_y, 2)
-        y_train = all_y_split_arr[0]
-        y_test = all_y_split_arr[1]
-        return x_train, x_test, y_train, y_test
-
-    def __prepare_x_y_data(self, normal_df: pd.DataFrame, mixed_df: pd.DataFrame,
-                           columns: List[str], step: int = 500, time_window: int = 1000,
-                           step4negative: int = 5, min_negative_last_chunk_size: int = 20):
-        negative_label_multivariate_split_data, neg_y = split_time_series_with_negative_labels(mixed_df[columns],
-                                                                                               mixed_df['Attack LABLE (1:No Attack, -1:Attack)'].values,
-                                                                                               step4negative, time_window, min_negative_last_chunk_size)
-
-        # note we can't do split_time_series_with_labels() on combined DF of both dataset as they aren't chronological.
-        mixed_label_multivariate_split_data, mixed_y = split_time_series_with_labels(mixed_df[columns],
-                                                                                     mixed_df['Attack LABLE (1:No Attack, -1:Attack)'].values,
-                                                                                     step, time_window)
-        norm_label_multivariate_split_data, norm_y = split_time_series_with_labels(normal_df[columns],
-                                                                                   np.ones(normal_df.shape[0]),
-                                                                                   step, time_window)
-        # number of samples is the third dimension
-        all_multivariate_prepared_x = np.concatenate((norm_label_multivariate_split_data,
-                                                      mixed_label_multivariate_split_data,
-                                                      negative_label_multivariate_split_data), axis=2)
-        all_multivariate_y = np.concatenate((norm_y, mixed_y, neg_y), axis=0)
-        # manual shuffle
-        np.random.seed(123)
-        permutation = np.random.permutation(all_multivariate_prepared_x.shape[2])
-        np.take(all_multivariate_prepared_x, permutation, axis=2, out=all_multivariate_prepared_x)
-        np.take(all_multivariate_y, permutation, axis=0, out=all_multivariate_y)
-        return all_multivariate_prepared_x, all_multivariate_y
+class ClassifierTestCase(unittest.TestCase):
 
     def __write_report(self, file_name, txt):
         with open(file_name, "w") as text_file:
@@ -99,6 +36,26 @@ class ClassifierTestCase1(unittest.TestCase):
         report = multi_var_shape_extractor.train_test_classifier_grid_search(get_forest_pipe_grid())
         self.__write_report('TestReports' + os.path.sep + 'FR_' + conf_test + '.txt', report)
 
+    def __run_svm(self, conf_test):
+        multi_var_shape_extractor = self.__get_shape_extractor(conf_test)
+        report = multi_var_shape_extractor.train_test_classifier_grid_search(get_svm_pipe_grid())
+        self.__write_report('TestReports' + os.path.sep + 'SVM_' + conf_test + '.txt', report)
+
+    def __run_gb(self, conf_test):
+        multi_var_shape_extractor = self.__get_shape_extractor(conf_test)
+        report = multi_var_shape_extractor.train_test_classifier_grid_search(get_gb_pipe_grid())
+        self.__write_report('TestReports' + os.path.sep + 'GB_' + conf_test + '.txt', report)
+
+    def __run_ada_b(self, conf_test):
+        multi_var_shape_extractor = self.__get_shape_extractor(conf_test)
+        report = multi_var_shape_extractor.train_test_classifier_grid_search(get_ada_boost_pipe_grid())
+        self.__write_report('TestReports' + os.path.sep + 'ADA_B_' + conf_test + '.txt', report)
+
+    def __run_knn(self, conf_test):
+        multi_var_shape_extractor = self.__get_shape_extractor(conf_test)
+        report = multi_var_shape_extractor.train_test_classifier_grid_search(get_knn_pipe_grid())
+        self.__write_report('TestReports' + os.path.sep + 'KNN_' + conf_test + '.txt', report)
+
     def __run_nn(self, conf_test):
         multi_var_shape_extractor = self.__get_shape_extractor(conf_test)
         report = multi_var_shape_extractor.train_test_classifier_grid_search(get_nn_pipe_grid())
@@ -114,56 +71,95 @@ class ClassifierTestCase1(unittest.TestCase):
         conf_test = "test_configuration_3_win_500"
         # self.__run_lg(conf_test)
         self.__run_nn(conf_test)
-        self.__run_forest(conf_test)
+        # self.__run_svm(conf_test)
+        # self.__run_ada_b(conf_test)
+        # self.__run_gb(conf_test)
+        # self.__run_knn(conf_test)
+        # self.__run_forest(conf_test)
 
     @ignore_warnings
     def test_configuration_3_win_500_25(self):
         conf_test = "test_configuration_3_win_500_25"
         # self.__run_lg(conf_test)
         self.__run_nn(conf_test)
-        self.__run_forest(conf_test)
+        # self.__run_svm(conf_test)
+        # self.__run_ada_b(conf_test)
+        # self.__run_gb(conf_test)
+        # self.__run_knn(conf_test)
+        # self.__run_forest(conf_test)
+
 
     @ignore_warnings
     def test_configuration_4_win_250(self):
         conf_test = "test_configuration_4_win_250"
         # self.__run_lg(conf_test)
         self.__run_nn(conf_test)
-        self.__run_forest(conf_test)
+        # self.__run_svm(conf_test)
+        # self.__run_ada_b(conf_test)
+        # self.__run_gb(conf_test)
+        # self.__run_knn(conf_test)
+        # self.__run_forest(conf_test)
+
 
     @ignore_warnings
     def test_configuration_4_win_250_25(self):
         conf_test = "test_configuration_4_win_250_25"
         # self.__run_lg(conf_test)
         self.__run_nn(conf_test)
-        self.__run_forest(conf_test)
+        # self.__run_svm(conf_test)
+        # self.__run_ada_b(conf_test)
+        # self.__run_gb(conf_test)
+        # self.__run_knn(conf_test)
+        # self.__run_forest(conf_test)
+
 
     @ignore_warnings
     def test_configuration_5_win_750(self):
         conf_test = "test_configuration_5_win_750"
         # self.__run_lg(conf_test)
         self.__run_nn(conf_test)
-        self.__run_forest(conf_test)
+        # self.__run_svm(conf_test)
+        # self.__run_ada_b(conf_test)
+        # self.__run_gb(conf_test)
+        # self.__run_knn(conf_test)
+        # self.__run_forest(conf_test)
+
 
     @ignore_warnings
     def test_configuration_5_win_750_25(self):
         conf_test = "test_configuration_5_win_750_25"
         # self.__run_lg(conf_test)
         self.__run_nn(conf_test)
-        self.__run_forest(conf_test)
+        # self.__run_svm(conf_test)
+        # self.__run_ada_b(conf_test)
+        # self.__run_gb(conf_test)
+        # self.__run_knn(conf_test)
+        # self.__run_forest(conf_test)
+
 
     @ignore_warnings
     def test_configuration_6_win_100(self):
         conf_test = "test_configuration_6_win_100"
         # self.__run_lg(conf_test)
         self.__run_nn(conf_test)
-        self.__run_forest(conf_test)
+        # self.__run_svm(conf_test)
+        # self.__run_ada_b(conf_test)
+        # self.__run_gb(conf_test)
+        # self.__run_knn(conf_test)
+        # self.__run_forest(conf_test)
+
 
     @ignore_warnings
     def test_configuration_6_win_100_25(self):
         conf_test = "test_configuration_6_win_100_25"
         # self.__run_lg(conf_test)
         self.__run_nn(conf_test)
-        self.__run_forest(conf_test)
+        # self.__run_svm(conf_test)
+        # self.__run_ada_b(conf_test)
+        # self.__run_gb(conf_test)
+        # self.__run_knn(conf_test)
+        # self.__run_forest(conf_test)
+
 
     @classmethod
     def setUpClass(cls):
